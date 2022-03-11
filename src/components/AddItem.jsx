3249a275154a35f7790bbf921f5c1c862d4af4b2
4vm <amadeumrzr@gmail.com>
2022-03-11 10:59:26 -0300

    AddItem component functions

--- src/components/AddItem.jsx
+++ src/components/AddItem.jsx
@@ -1,18 +1,56 @@
 import { useState } from 'react'
 
-const AddItem = () => {
+const AddItem = (props) => {
   const [enteredType, setEnteredType] = useState()
   const [enteredTitle, setEnteredTitle] = useState('')
   const [enteredAmount, setEnteredAmount] = useState('')
+  const [enteredDate, setEnteredDate] = useState('')
+
+  const typeChangeHandler = (event) => {
+    setEnteredType(event.target.value)
+    console.log(enteredType)
+  }
+
+  const titleChangehandler = (event) => {
+    setEnteredTitle(event.target.value)
+    console.log(enteredTitle)
+  }
+
+  const amountChangeHandler = (event) => {
+    setEnteredAmount(event.target.value)
+    console.log(enteredAmount)
+  }
+
+  const dateChangeHandler = (event) => {
+    setEnteredDate(new Date())
+    console.log(enteredDate)
+  }
+
+  const submitHandler = (event) => {
+    event.preventDefault()
+    const enteredData = {
+      id: Math.random().toString(),
+      type: enteredType,
+      title: enteredTitle,
+      amount: enteredAmount,
+      date: enteredDate,
+    }
+    props.onDataHandler(enteredData)
+  }
 
   return (
-    <form onSubmit>
-      <select>
-        <option>Despesas</option>
-        <option>Proventos</option>
+    <form onSubmit={submitHandler}>
+      <select onChange={typeChangeHandler}>
+        <option value="despensa">Despesa</option>
+        <option value="ganho">Ganho</option>
       </select>
-      <input type="text" placeholder="Descrição" />
-      <input type="text" placeholder="Valor" />
+      <input
+        type="text"
+        placeholder="Descrição"
+        onChange={titleChangehandler}
+      />
+      <input type="text" placeholder="Valor" onChange={amountChangeHandler} />
+      <input type="date" onChange={dateChangeHandler} />
       <button>Adicionar</button>
     </form>
   )
